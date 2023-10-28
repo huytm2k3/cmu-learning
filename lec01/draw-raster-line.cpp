3dcb61ac7967638c478778f785913cf222f12ead
Ta Minh Huy <huytm2k3@gmail.com>
2023-10-28 17:10:10 +0700

    descripting showCoordinatesRasterLine

--- lec01/draw-raster-line.cpp
+++ lec01/draw-raster-line.cpp
@@ -9,6 +9,17 @@ int main()
     std::cout << "Input coordinates of second endpoint (u2, v2): ";
     std::cin >> u2 >> v2;
 
+    showCoordinatesRasterLine(u1, v1, u2, v2);
+    return 0;
+}
+
+// @brief Displays the raster coordinates of a line between two points.
+// @param u1: x-coordinate of the first endpoint.
+// @param v1: y-coordinate of the first endpoint.
+// @param u2: x-coordinate of the second endpoint.
+// @param v2: y-coordinate of the second endpoint.
+void showCoordinatesRasterLine(float u1, float v1, float u2, float v2)
+{
     float s = (u2 - u1) / (v2 - v1);
     float v = v1;
     std::cout << "Slope: " << (((u1 < u2) && (v1 < v2)) ? "Upper-right" : "Upper-left") << std::endl;
@@ -18,5 +29,4 @@ int main()
         v += s;
         std::cout << "(" << u << "," << round(v) << ")";
     }
-    return 0;
 }
